6233153020264b9c0e3e4ead30a286e7f2807d34
bryan lesht <bryan.lesht@colorado.edu>
2020-02-20 23:22:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  pop = string_array.collect {|word| "#{word.reverse}!"}
+  pop = string_array.collect {|word| word.reverse}
   p pop 
  
   
