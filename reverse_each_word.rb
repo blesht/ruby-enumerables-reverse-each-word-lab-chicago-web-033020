8563ceab00e6404d4ec5b5bf1b48ec22c87c6966
bryan lesht <bryan.lesht@colorado.edu>
2020-02-20 23:31:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   #pop = string_array.collect {|word| word.reverse}
   #pop.join(" ")
-  i = 0 
+  
   each_array = []
-  while i < string_array.length do 
-    string_array.each {|word| each_array << word.reverse}
-    i += 1 
-  end 
+  
+  string_array.each {|word| each_array << word.reverse}
+  
+  
   p each_array
  
   
